e81240c75516a294ad2ddf60ad51fe5309e3b6e2
yoel <yoel@macbook-pro.home>
2020-08-26 22:09:09 -0700

    [reactcalculator] add zero condition and zero button conditional to css

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,7 +3,9 @@ import React from 'react';
 import "./Button.css"
 
 const Button = ({content}) => {
-  return <div className="Button">{content}</div>
+  return <div 
+  className={`Button ${content === '0' ? 'zero' : ''}`}>{content}
+  </div>
 };
 
 export default Button;
