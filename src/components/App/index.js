03afb63a1914b902ff87028e59775baf15546a6d
yoel <yoel@MacBook-Pro.local>
2020-08-27 02:06:40 -0700

    [reactcalculator] removed menu img was err on page

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import menu from '../../assets/menu.png';
+import React, { useEffect, useState } from "react";
 import Button from "../Button";
 import commafy from "../utils/commafy";
-import './App.css';
-
+import "./App.css";
 
 const App = () => {
   const [time, setTime] = useState(new Date());
@@ -14,123 +12,119 @@ const App = () => {
   useEffect(() => {
     setTime(new Date());
   }, [new Date().getMinutes()]);
-// TODO: figure out how to get time to work
 
   const handleButtonPress = content => () => {
     const num = parseFloat(value);
 
-    if (content === 'AC') {
-      setValue('0');
+    if (content === "AC") {
+      setValue("0");
       setMemory(null);
       setOperator(null);
       return;
     }
 
-    if (content === '±') {
+    if (content === "±") {
       setValue((num * -1).toString());
       return;
     }
 
-    if (content === '%') {
+    if (content === "%") {
       setValue((num / 100).toString());
       setMemory(null);
       setOperator(null);
       return;
     }
 
-    if (content === '.') {
-      if (value.includes('.')) return;
+    if (content === ".") {
+      if (value.includes(".")) return;
 
-      setValue(value + '.');
+      setValue(value + ".");
       return;
     }
 
-    if (content === '+') {
-      if(operator !== null) {
-        if (operator === '+') {
+    if (content === "+") {
+      if (operator !== null) {
+        if (operator === "+") {
           setMemory(memory + parseFloat(value));
-        } else if (operator === '−') {
+        } else if (operator === "−") {
           setMemory(memory - parseFloat(value));
-        } else if (operator === '×') {
+        } else if (operator === "×") {
           setMemory(memory * parseFloat(value));
-        } else if (operator === '÷') {
+        } else if (operator === "÷") {
           setMemory(memory / parseFloat(value));
         }
       } else {
-      setMemory(parseFloat(value));
+        setMemory(parseFloat(value));
       }
       setValue("0");
-      setOperator("+")
+      setOperator("+");
       return;
     }
-
-    if (content === '−') {
-      if(operator !== null) {
-        if (operator === '+') {
+    if (content === "−") {
+      if (operator !== null) {
+        if (operator === "+") {
           setMemory(memory + parseFloat(value));
-        } else if (operator === '−') {
+        } else if (operator === "−") {
           setMemory(memory - parseFloat(value));
-        } else if (operator === '×') {
+        } else if (operator === "×") {
           setMemory(memory * parseFloat(value));
-        } else if (operator === '÷') {
+        } else if (operator === "÷") {
           setMemory(memory / parseFloat(value));
         }
       } else {
-      setMemory(parseFloat(value));
+        setMemory(parseFloat(value));
       }
       setValue("0");
       setOperator("−");
       return;
     }
-
-    if (content === '×') {
-      if(operator !== null) {
-        if (operator === '+') {
+    if (content === "×") {
+      if (operator !== null) {
+        if (operator === "+") {
           setMemory(memory + parseFloat(value));
-        } else if (operator === '−') {
+        } else if (operator === "−") {
           setMemory(memory - parseFloat(value));
-        } else if (operator === '×') {
+        } else if (operator === "×") {
           setMemory(memory * parseFloat(value));
-        } else if (operator === '÷') {
+        } else if (operator === "÷") {
           setMemory(memory / parseFloat(value));
         }
       } else {
-      setMemory(parseFloat(value));
+        setMemory(parseFloat(value));
       }
       setValue("0");
       setOperator("×");
       return;
     }
-
-    if (content === '÷') {
-      if(operator !== null) {
-        if (operator === '+') {
+    if (content === "÷") {
+      if (operator !== null) {
+        if (operator === "+") {
           setMemory(memory + parseFloat(value));
-        } else if (operator === '−') {
+        } else if (operator === "−") {
           setMemory(memory - parseFloat(value));
-        } else if (operator === '×') {
+        } else if (operator === "×") {
           setMemory(memory * parseFloat(value));
-        } else if (operator === '÷') {
+        } else if (operator === "÷") {
           setMemory(memory / parseFloat(value));
         }
       } else {
-      setMemory(parseFloat(value));
+        setMemory(parseFloat(value));
       }
       setValue("0");
       setOperator("÷");
       return;
     }
 
-    if (content === '=') {
+    if (content === "=") {
       if (!operator) return;
 
-      if (operator === '+') {
+      if (operator === "+") {
         setValue((memory + parseFloat(value)).toString());
-      } else if (operator === '−') {
+      } else if (operator === "−") {
         setValue((memory - parseFloat(value)).toString());
-      } else if (operator === '×') {
+      } else if (operator === "×") {
         setValue((memory * parseFloat(value)).toString());
-      } else if (operator === '÷') {
+      } else if (operator === "÷") {
         setValue((memory / parseFloat(value)).toString());
       }
       setMemory(null);
@@ -138,56 +132,57 @@ const App = () => {
       return;
     }
 
-    if (value[value.length - 1] === '.') {
+    if (value[value.length - 1] === ".") {
       setValue(value + content);
     } else {
-    setValue((parseFloat(num + content)).toString());
-
+      setValue(parseFloat(num + content).toString());
     }
-
   };
 
-  return <div className="App">
-    <div className="top">
-      <div className="time">
-      {time
-      .getHours()
-      .toString()
-      .padStart(2, 0)}
-      :
-      {time
-      .getMinutes()
-      .toString()
-      .padStart(2, 0)}
+  return (
+    <div className="App">
+      <div className="top">
+        <div className="time">
+          {time
+            .getHours()
+            .toString()
+            .padStart(2, "0")}
+          :
+          {time
+            .getMinutes()
+            .toString()
+            .padStart(2, "0")}
+        </div>
       </div>
-      <div className="menu">
-        <img src={menu} alt="menu" />
+      <div className="display">{commafy(value)}</div>
+      <div className="buttons">
+        <Button
+          onButtonClick={handleButtonPress}
+          content="AC"
+          type="function"
+        />
+        <Button onButtonClick={handleButtonPress} content="±" type="function" />
+        <Button onButtonClick={handleButtonPress} content="%" type="function" />
+        <Button onButtonClick={handleButtonPress} content="÷" type="operator" />
+        <Button onButtonClick={handleButtonPress} content="7" />
+        <Button onButtonClick={handleButtonPress} content="8" />
+        <Button onButtonClick={handleButtonPress} content="9" />
+        <Button onButtonClick={handleButtonPress} content="×" type="operator" />
+        <Button onButtonClick={handleButtonPress} content="4" />
+        <Button onButtonClick={handleButtonPress} content="5" />
+        <Button onButtonClick={handleButtonPress} content="6" />
+        <Button onButtonClick={handleButtonPress} content="−" type="operator" />
+        <Button onButtonClick={handleButtonPress} content="1" />
+        <Button onButtonClick={handleButtonPress} content="2" />
+        <Button onButtonClick={handleButtonPress} content="3" />
+        <Button onButtonClick={handleButtonPress} content="+" type="operator" />
+        <Button onButtonClick={handleButtonPress} content="0" />
+        <Button onButtonClick={handleButtonPress} content="." />
+        <Button onButtonClick={handleButtonPress} content="=" type="operator" />
       </div>
+      <div className="bottom" />
     </div>
-    <div className="display">{commafy(value)}</div>
-    <div className="buttons">
-      <Button onButtonClick={handleButtonPress} content="AC" type="function"/>
-      <Button onButtonClick={handleButtonPress} content="±" type="function"/>
-      <Button onButtonClick={handleButtonPress} content="%" type="function"/>
-      <Button onButtonClick={handleButtonPress} content="÷" type="operator"/>
-      <Button onButtonClick={handleButtonPress} content="7"/>
-      <Button onButtonClick={handleButtonPress} content="8"/>
-      <Button onButtonClick={handleButtonPress} content="9"/>
-      <Button onButtonClick={handleButtonPress} content="×" type="operator"/>
-      <Button onButtonClick={handleButtonPress} content="4"/>
-      <Button onButtonClick={handleButtonPress} content="5"/>
-      <Button onButtonClick={handleButtonPress} content="6"/>
-      <Button onButtonClick={handleButtonPress} content="−" type="operator"/>
-      <Button onButtonClick={handleButtonPress} content="1"/>
-      <Button onButtonClick={handleButtonPress} content="2"/>
-      <Button onButtonClick={handleButtonPress} content="3"/>
-      <Button onButtonClick={handleButtonPress} content="+" type="operator"/>
-      <Button onButtonClick={handleButtonPress} content="0"/>
-      <Button onButtonClick={handleButtonPress} content="."/>
-      <Button onButtonClick={handleButtonPress} content="=" type="operator"/>
-    </div>
-    <div className="bottom" />
-  </div>
+  );
 };
 
 export default App;
